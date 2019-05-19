41d277e3486f2436cf41e8516bafb933d0293dff
Crowdbotics <team@crowdbotics.com>
2019-05-19 15:59:20 +0000

    initial commit

---
+++ android/app/src/main/java/com/decoded_3541/MainActivity.java
@@ -0,0 +1,15 @@
+package com.decoded_3541;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "decoded_3541";
+    }
+}
